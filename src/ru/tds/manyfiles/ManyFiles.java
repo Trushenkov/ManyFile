cb3634b71f47dd5056684e11c5f25202e9f59669
Трушенков Дмитрий <diman28051999@gmail.com>
2017-04-30 02:53:52 +0300

    Добавлен комментарий к классу ManyFiles.

--- src/ru/tds/manyfiles/ManyFiles.java
+++ src/ru/tds/manyfiles/ManyFiles.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Random;
 
 /**
- * Программа "Много файлов." Реализовано независимо работающие классы для решения отдельных этапов задачи.
+ * Класс для создания файла целых чисел intdata.dat программным путём и заполнения его с помощью рандома.
  *
  * @author Трушенков Дмитрий 15ОИТ18
  */
@@ -12,7 +12,7 @@ public class ManyFiles {
     public static void main(String[] args) throws IOException {
         Random random = new Random();
         DataOutputStream writeNumbers = new DataOutputStream(new FileOutputStream(new File("intdata.dat")));
-        for (int i = 0; i < 35; i++) {
+        for (int i = 1; i < 150; i++) {
             writeNumbers.writeInt(random.nextInt(999999));
         }
         writeNumbers.close();
