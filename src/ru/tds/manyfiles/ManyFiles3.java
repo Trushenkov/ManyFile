f3e5a0d013e9ee4655d5890d2b15b7aa943f9400
Трушенков Дмитрий <diman28051999@gmail.com>
2017-05-21 22:51:36 +0300

    Исправлен комментарий к методу isLuckyTicket и заменил  DataOutputStream и DataInputStream на BufferedWriter и BufferedReader, так как теперь исходный файл с номерами билетов формата ".txt".

--- src/ru/tds/manyfiles/ManyFiles3.java
+++ src/ru/tds/manyfiles/ManyFiles3.java
@@ -3,40 +3,39 @@ package ru.tds.manyfiles;
 import java.io.*;
 
 /**
- * Класс для создания текстового файла, содержашего только счастливые номера
+ * Класс для создания текстового файла, содержашего только счастливые билеты
  * (сумма левых 3-х цифр равна сумме правых 3-х цифр), на основе файла int6data.dat
  *
  * @author Trushenkov Dmitry 15ОИТ18.
  */
 public class ManyFiles3 {
-
     public static void main(String[] args) throws IOException {
-        DataInputStream reader = new DataInputStream(new FileInputStream(new File("int6data.dat")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("int6data.dat")));
         BufferedWriter writer = new BufferedWriter(new FileWriter(new File("txt6data.txt")));
-        for (int i = 1; i < 150; i++) {
-            int number = reader.readInt();
-            if (isLuckyTicket(number)) {
-                writer.write(String.valueOf(number));
-                writer.write("\r\n");
+            for (int i = 1; i < 11; i++) {
+                String number = reader.readLine();
+                if (isLuckyTicket(number)) {
+                    writer.write(String.valueOf(number));
+                    writer.write("\r\n");
+                }
             }
-        }
         reader.close();
         writer.close();
     }
 
     /**
-     * Метод для проверки числа на то, является ли число счастливым номером.
+     * Метод для проверки числа на то, является ли оно счастливым билетом или нет.
      *
      * @param number число
      * @return true, если сумма левых 3-х цифр равна сумме правых 3-х цифр.
      */
-    private static boolean isLuckyTicket(int number) {
-        int number1 = number / 100000;
-        int number2 = (number / 10000) % 10;
-        int number3 = (number / 1000) % 10 % 10;
-        int number4 = (number / 100) % 10 % 10 % 10;
-        int number5 = (number / 10) % 10 % 10 % 10 % 10;
-        int number6 = number % 10 % 10 % 10 % 10;
+    private static boolean isLuckyTicket(String number) {
+        int number1 = Integer.parseInt(number) / 100000;
+        int number2 = (Integer.parseInt(number) / 10000) % 10;
+        int number3 = (Integer.parseInt(number) / 1000) % 10 % 10;
+        int number4 = (Integer.parseInt(number) / 100) % 10 % 10 % 10;
+        int number5 = (Integer.parseInt(number) / 10) % 10 % 10 % 10 % 10;
+        int number6 = Integer.parseInt(number) % 10 % 10 % 10 % 10;
         int sum1 = number1 + number2 + number3;
         int sum2 = number4 + number5 + number6;
         return (sum1 == sum2);
