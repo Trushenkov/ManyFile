d396685c1319c9602b99d975c986c7bf48e09a0a
Трушенков Дмитрий <diman28051999@gmail.com>
2017-05-22 22:57:51 +0300

    Для чтения из файла целых чисел используется DataInputStream, а  для записи счастливых билетов в текстовый файл используется BufferedWriter.

--- src/ru/tds/manyfiles/ManyFiles3.java
+++ src/ru/tds/manyfiles/ManyFiles3.java
@@ -10,15 +10,15 @@ import java.io.*;
  */
 public class ManyFiles3 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("int6data.dat")));
+        DataInputStream reader = new DataInputStream(new FileInputStream(new File("int6data.dat")));
         BufferedWriter writer = new BufferedWriter(new FileWriter(new File("txt6data.txt")));
-            for (int i = 1; i < 11; i++) {
-                String number = reader.readLine();
-                if (isLuckyTicket(number)) {
-                    writer.write(String.valueOf(number));
-                    writer.write("\r\n");
-                }
+        for (int i = 1; i < 13; i++) {
+            int number = reader.readInt();
+            if (isLuckyTicket(number)) {
+                writer.write(String.valueOf(number));
+                writer.write("\r\n");
             }
+        }
         reader.close();
         writer.close();
     }
@@ -29,13 +29,13 @@ public class ManyFiles3 {
      * @param number число
      * @return true, если сумма левых 3-х цифр равна сумме правых 3-х цифр.
      */
-    private static boolean isLuckyTicket(String number) {
-        int number1 = Integer.parseInt(number) / 100000;
-        int number2 = (Integer.parseInt(number) / 10000) % 10;
-        int number3 = (Integer.parseInt(number) / 1000) % 10 % 10;
-        int number4 = (Integer.parseInt(number) / 100) % 10 % 10 % 10;
-        int number5 = (Integer.parseInt(number) / 10) % 10 % 10 % 10 % 10;
-        int number6 = Integer.parseInt(number) % 10 % 10 % 10 % 10;
+    private static boolean isLuckyTicket(int number) {
+        int number1 = number / 100000;
+        int number2 = number / 10000 % 10;
+        int number3 = number / 1000 % 10 % 10;
+        int number4 = number / 100 % 10 % 10 % 10;
+        int number5 = number / 10 % 10 % 10 % 10 % 10;
+        int number6 = number % 10 % 10 % 10 % 10;
         int sum1 = number1 + number2 + number3;
         int sum2 = number4 + number5 + number6;
         return (sum1 == sum2);
