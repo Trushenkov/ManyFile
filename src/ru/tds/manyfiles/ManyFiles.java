fd3b3c434be6ea2b0da7b3a29b11422f8e2d9ad3
Трушенков Дмитрий <diman28051999@gmail.com>
2017-05-22 22:48:19 +0300

    Исходные данные читаются из файла intdata.txt и записываются программным путем в файл целых чисел intdata.dat.

--- src/ru/tds/manyfiles/ManyFiles.java
+++ src/ru/tds/manyfiles/ManyFiles.java
@@ -4,18 +4,20 @@ import java.io.*;
 import java.util.Random;
 
 /**
- * Класс для создания файла целых чисел intdata.dat программным путём и заполнения его с помощью рандома.
+ * Класс для создания файла целых чисел intdata.dat программным путём.
  *
  * @author Трушенков Дмитрий 15ОИТ18
  */
 public class ManyFiles {
     public static void main(String[] args) throws IOException {
-        Random random = new Random();
-        DataOutputStream writeNumbers = new DataOutputStream(new FileOutputStream(new File("intdata.dat")));
-        for (int i = 1; i < 150; i++) {
-            writeNumbers.writeInt(random.nextInt(999999));
+        DataOutputStream writer = new DataOutputStream(new FileOutputStream(new File("intdata.dat")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("intdata.txt")));
+        String string;
+        while ((string = reader.readLine()) != null) {
+            writer.writeInt(Integer.valueOf(string));
         }
-        writeNumbers.close();
+        reader.close();
+        writer.close();
     }
 }
 
